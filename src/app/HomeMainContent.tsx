f3fd5f33050c23fd3c6711ba2891b7dc632f3469
Fassih Haider <fassih.haider786@gmail.com>
2024-03-31 06:15:35 +0500

    style: fix home page country layout

--- src/app/HomeMainContent.tsx
+++ src/app/HomeMainContent.tsx
@@ -75,7 +75,7 @@ export const HomeMainContent = ({ countries }: TProps) => {
                 selectedValue={dropdownValue}
             />
 
-            <section className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3">
+            <section className="grid grid-cols-1 md:grid-cols-3 gap-4 lg:grid-cols-4">
                 {countriesToRender}
             </section>
         </>
--- src/components/molecules/CountryCard.tsx
+++ src/components/molecules/CountryCard.tsx
@@ -28,7 +28,7 @@ export const CountryCard = ({
 
     return (
         <div
-            className="shadow-md hover:scale-[102%] transition cursor-pointer w-full border dark:border-dark-blue"
+            className="shadow-md hover:scale-[102%] transition cursor-pointer w-full border dark:border-dark-blue rounded-md overflow-hidden"
             onClick={onClickCard}
         >
             <div className="overflow-hidden rounded-t-md">
@@ -39,7 +39,7 @@ export const CountryCard = ({
                     height={300}
                 />
             </div>
-            <div className="p-6 dark:bg-dark-blue rounded-b-md">
+            <div className="p-6 dark:bg-dark-blue rounded-b-md h-full">
                 <h3 className="text-lg font-bold mb-4 md:text-xl">{name}</h3>
                 <StatRow title="Population" value={population} />
                 <StatRow title="Region" value={region} />
